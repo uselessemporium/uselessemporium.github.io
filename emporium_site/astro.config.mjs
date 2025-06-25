ced8f5f7320be1456d6751c63cafa587d737eae6
PakkanenAnastacia <aatevez@gmail.com>
2025-06-25 07:37:31 -0300

    Added action!

--- emporium_site/astro.config.mjs
+++ emporium_site/astro.config.mjs
@@ -1,5 +1,18 @@
 import { defineConfig } from 'astro/config'
 
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const LOCALHOST_PORT = 4321;
+const LOCALHOST_URL = `https://localhost:${LOCALHOST_PORT}`;
+const LIVE_URL = 'https://uselessemporium.github.io';
+
+let BASE_URL = LOCALHOST_URL;
+
+const isBuild = SCRIPT.includes("astro build");
+
+if(isBuild){
+  BASE_URL = LIVE_URL
+}
+
 export default defineConfig({
-  site: 'https://uselessemporium.github.io'
-})
+  site: BASE_URL
+});
